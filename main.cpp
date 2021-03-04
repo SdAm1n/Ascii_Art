986f44893ce2903c81654f2c2ee92f0fbe431fc0
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2021-03-05 01:24:32 +0600

    added comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/*This code is made available under the Creative Commons Zero 1.0 License 
+(https://creativecommons.org/publicdomain/zero/1.0)*/
+
 /*
     ASCII ART USING OPENCV WITH C++
     CREATED BY: sdam1n
